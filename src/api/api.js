663d7306e0cb47738947d3319df798ebb06f3a45
GriEvouss <majorov2005@gmail.com>
2024-12-21 14:30:04 +0300

    Final

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 export const config = {
-    baseUrl: "https://nomoreparties.co/v1/frontend-st-cohort-201",
+    baseUrl: "https://nomoreparties.co/v1/apf-cohort-202",
     headers: {
-        authorization: "d470e4e0-7e0b-4ca0-a070-d367ecf5064e",
+        authorization: "440187fc-a974-4d7a-9fc7-a31886563500",
         "Content-Type": "application/json",
     },
 };
